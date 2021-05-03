5a75a11800f8856d96decb26edec018b0f0262e2
emaden99 <79649708+emaden99@users.noreply.github.com>
2021-05-03 19:57:38 +0300

    Add files via upload
    Our team page uploaded

---
+++ signin.js
@@ -0,0 +1,21 @@
+function useradd(){
+   localStorage.setItem("userin",document.getElementById("username").value);
+}
+function setUser() {
+    if(localStorage.getItem("userin")!==null){
+        document.getElementById("navbar-content-right").innerHTML=
+        " <a href=#><i class=fa fa-fw fa-user></i> "+localStorage.getItem("userin")+"</a> "+
+        " <a  onclick=userout() ><i class=fa fa-fw fa-user  ></i> Sign Out</a> "
+       
+    }else {
+    document.getElementById("navbar-content-right").innerHTML=
+    " <a href=#><i class=fa fa-fw fa-user></i> Sign Up</a> "+
+    " <a href=#><i class=fa fa-fw fa-user></i> Sign In</a> "
+    }
+}
+
+function userout(){
+localStorage.removeItem("userin");
+window.location.reload(false);
+
+}
