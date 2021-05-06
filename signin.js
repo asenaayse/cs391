046bd9d348e41fe617a56d510b571766068ea070
kaanapak <80036646+kaanapak@users.noreply.github.com>
2021-05-06 10:52:01 +0300

    Update signin.js

--- signin.js
+++ signin.js
@@ -1,7 +1,12 @@
 function useradd(){
+   if(localStorage){
    localStorage.setItem("userin",document.getElementById("username").value);
 }
+}else{
+   alert("Sorry,your browser do not support local storage"
+}
 function setUser() {
+      if(localStorage){
     if(localStorage.getItem("userin")!==null){
         document.getElementById("navbar-content-right").innerHTML=
         " <a href=#><i class=fa fa-fw fa-user></i> "+localStorage.getItem("userin")+"</a> "+
@@ -12,6 +17,7 @@ function setUser() {
     ' <a href="signup.html"><i class=fa fa-fw fa-user></i> Sign Up</a> '+
     ' <a href="signin.html"><i class=fa fa-fw fa-user></i> Sign In</a> '
     }
+      }
 }
 
 function userout(){
